24258bd3a022fe63147e65a21c35c0bd2c464b23
H.Rajapaksha <it20179458@my.sliit.lk>
2021-12-06 17:38:36 +0530

    add new web project

--- src/assets/data/webprojects.js
+++ src/assets/data/webprojects.js
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import delight8 from '../images/delight8.jpg';
 import travelnet from '../images/travelnet.jpg';
+import vstoreapp from '../images/vstoreapp.jpg';
 
 const projects = [
   {
@@ -15,6 +16,13 @@ const projects = [
     name: 'Online Travel Guide System',
     desc: 'University project for creating a web application using HTML, CSS, JS, PHP, and PHP MyAdmin',
     img: travelnet,
+    // link: 'https://www.facebook.com/R.HarshanaOnline/videos/410584440735342',
+  },
+  {
+    id: uuidv4(),
+    name: 'Online E-Commerce App',
+    desc: 'The E-Commerce android application ‘V-Store’ was developed for the customers to safely purchase their essentials since they are confined to their homes due to COVID-19',
+    img: vstoreapp,
     // link: 'https://www.facebook.com/R.HarshanaOnline/videos/410584440735342',
   },
 ];
